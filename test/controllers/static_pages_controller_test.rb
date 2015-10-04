a0c4d04af0b49300553b57198d90aaab321f8896
selfup <boudinot.regis@yahoo.com>
2015-10-04 17:04:14 -0600

    Added integration test, extra view test

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -9,19 +9,25 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | #{@base_title}"
+    assert_select "title", ""
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | #{@base_title}"
+    assert_select "title", "Help"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | #{@base_title}"
+    assert_select "title", "About"
+  end
+
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact"
   end
 
 end
