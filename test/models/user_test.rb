17a6bb06da57bfd6271127857b4d68104f418938
selfup <boudinot.regis@yahoo.com>
2015-10-04 19:14:18 -0600

    Added bcrypt and increases password and email security

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,7 +2,8 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user = User.new(name: "Example user", email: "user@example.com")
+    @user = User.new(name: "Example user", email: "user@example.com",
+                     password: "wowowow", password_confirmation: "wowowow")
   end
 
   test "should be valid" do
@@ -43,5 +44,15 @@ class UserTest < ActiveSupport::TestCase
     duplicate_user.email = @user.email.upcase
     @user.save
     assert_not duplicate_user.valid?
+  end
+
+  test "password should be present (nonblank)" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
   end
 end
