4338e94833c8419a07844dd918d653ec5f0a8115
selfup <boudinot.regis@yahoo.com>
2015-10-05 12:44:52 -0600

    Added user name and email to show view - added gravatar functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,13 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
 
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find_by(params[:id])
   end
 end
